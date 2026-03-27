0a4407009a678f2705660799f1f2b07738de812d
oceane <oceane.gomez@esiroi.re>
2026-03-27 07:41:22 +0000

    Ajout fonctions dans .h

--- src/messStacker.h
+++ src/messStacker.h
@@ -8,5 +8,20 @@
 
 const char* getMessStackerVersion(void);
 
+bool sendMessage(uint8_t  cmd, char data[SIZE_MAX_DATA], uint8_t size);
+
+bool haveMessage(void);
+
+bool nextMessage(void);
+
+uint8_t curMessageCmd(void);
+
+uint8_t curMessageSize(void);
+
+uint8_t curMessageChecksum(void);
+
+bool curMessageData(int lenghtMax, char buff);
+
+uint8_t checksumMessage(uint8_t  cmd, char data[SIZE_MAX_DATA], uint8_t size);
 
 #endif
